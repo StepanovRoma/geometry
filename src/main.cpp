c3d4df690d544bca2d9a62906fec3d083126589a
StepanovRoma <stepanovroma2019@gmail.com>
2020-05-15 14:17:09 +0700

    added main func

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include "function.h"
+#include "Coord.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float x, y, r;
+
+    string str;
+    cout << "Correct imput example: (x y, r)" << endl;
+    cout << "Enter coord of 1 circle: ";
+    getline(cin, str, '\n');
+    if (Coord(str, x, y, r)) {
+        cout << "1. ";
+        cout << "x = " << x << ", y = " << y << ", r = " << r;
+
+        cout << "\nS: " << S(r) << "\nP: " << P(r) << endl;
+    } else {
+        cout << "Ooops...";
+    }
+    return 0;
+}
